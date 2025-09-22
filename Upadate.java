7b62d5b1fcee79bff2c597ba0fe70581ea891c68
ubNishant <ubnishant21@gmail.com>
2025-09-23 01:32:20 +0530

    Added Upadate.java

---
+++ Upadate.java
@@ -0,0 +1,13 @@
+public class Upadate {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int indexpos = 2;
+        int element = 69;
+        arr[indexpos] = element;
+        for (int num : arr) {
+            System.out.println(num);
+        }
+
+    }
+
+}
