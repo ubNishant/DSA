85bcaee92edd9a400fd0bfc95875da085a75c546
ubNishant <ubnishant21@gmail.com>
2025-09-22 15:56:07 +0530

    Added Arr.java

---
+++ Arr.java
@@ -0,0 +1,18 @@
+class Arr {
+
+    public static void main(String[] args) {
+        // int arr[] = new int[5];
+        // arr[0] = 2;
+
+        // System.err.println(arr[0]);
+        int arr[] = { 2, 4, 5, 6, 8 };
+
+        // for (int i = 0; i < arr.length; i++) {
+        // System.err.println(arr[i]);
+        // }
+        for (int no : arr) {
+            System.out.println(no);
+        }
+    }
+
+}
