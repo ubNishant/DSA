155147de3ddd110ca59da0544f622f1c9719ae81
ubNishant <ubnishant21@gmail.com>
2025-10-06 23:56:10 +0530

    StringBuild.java

---
+++ StringBuild.java
@@ -0,0 +1,10 @@
+public class StringBuild {
+
+    public static void main(String args[]) {
+        String str = "Nishant";
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+    }
+
+}
